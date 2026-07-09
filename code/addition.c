24cfc64afcf1ba3dd744ef5c4774f6ac7432fe31
axionb21 <utkarshbabar2006@gmail.com>
2026-07-09 09:56:53 +0530

    adding basic C program

---
+++ code/addition.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+
+int main (){
+
+ int a , b ;
+ float c , d ;
+
+ printf("enter the value of a = ");
+ scanf("%d",&a,&c);
+ printf("enter the value of b = ");
+ scanf("%d",&b,&d);
+ printf("the sum is %d\n", a+b );
+ printf("the sub %d\n",a-b);
+ printf("the mult %d \n", a * b);
+ printf("the divi %f\n", c /d);
+
+
+ return 0;
+
+}
+
---
+++ code/varibles.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+ int a = 3;
+ char b[] = "utkarsh" ;   
+ bool areYouright = true;
+ 
+
+ printf("number is %d \n",a);        
+ printf("hello %s \n" ,b);
+ printf("the value is %d \n ", areYouright);
+
+
+
+
+
+return 0;
+}
